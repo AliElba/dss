ed182c296b3f27fa737155b372b860d7f7e85038
Ali Elba <aly.elba@accenture.com>
2024-03-25 14:18:04 +0100

    change dimension input field into area
    instead of numOptions -> alternatives by default 3
    
    can sort by weight starting with less weight
    
    rename into Qty. Structure System
    
    by default sort by less weight (less cost)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { Component, inject, signal } from '@angular/core';
+import { AfterViewInit, Component, inject, signal, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { MatTableModule } from '@angular/material/table';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
@@ -9,16 +9,21 @@ import { DATA_SOURCE, Option } from './datasource';
 import { MatCheckbox } from '@angular/material/checkbox';
 import { BestOption, Priority } from './types';
 import { BestOptionService } from './best-option.service';
+import { MatSort, MatSortModule } from '@angular/material/sort';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, MatInputModule, MatButtonModule, MatTableModule, FormsModule, MatCheckbox],
+  imports: [RouterOutlet, CommonModule, MatInputModule, MatButtonModule, MatTableModule, MatSortModule, FormsModule, MatCheckbox],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   bestOptionService = inject(BestOptionService);
+
+  @ViewChild(MatSort) matSort!: MatSort;
+  dataSource = new MatTableDataSource(new Array<BestOption>());
+
   displayedColumns: Array<keyof Option | keyof BestOption> = [
     'system',
     'id_main',
@@ -34,43 +39,64 @@ export class AppComponent {
     'difference_area',
     'difference_energy',
   ];
-  bestOptions = signal<BestOption[]>([]);
   exactMatch = signal<boolean>(true);
   needed_area_m2?: number;
-  needed_energy?: number = 2000;
-  numOptions = 5;
+  needed_energy?: number = 5000;
+  numOfAlternatives = 10;
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.matSort;
+
+    this.dataSource.sortingDataAccessor = (item, property: string) => {
+      switch (property) {
+        case 'weight':
+          return item.option.weight;
+        case 'energy':
+          return item.option.energy;
+        case 'no_solar_panel':
+          return item.option.no_solar_panel;
+        case 'area_m2':
+          return item.option.area_m2;
+        // Add cases for other nested properties if needed
+        default:
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-expect-error
+          return item[property];
+      }
+    };
+  }
 
   onFindBestOptions() {
-    this.bestOptions.set(
-      this.bestOptionService.findBestOptions(
-        DATA_SOURCE,
-        this.numOptions,
-        this.exactMatch(),
-        this.needed_area_m2,
-        this.needed_energy
-      )
+    this.dataSource.data = this.bestOptionService.findBestOptions(
+      [...DATA_SOURCE],
+      this.numOfAlternatives,
+      this.exactMatch(),
+      this.needed_area_m2,
+      this.needed_energy
     );
+
+    if (this.dataSource.data.length > 0 && !this.dataSource.sort?.active) {
+      this.dataSource.sort?.sort({ id: 'weight', start: 'asc', disableClear: true });
+    }
   }
 
   sortBestOptions(priority: Priority) {
-    this.bestOptions.update((x) => {
-      return [...x].sort((a, b) => {
-        if (priority === 'energy') {
-          /**
-           * To convert an array containing both positive and negative numbers to an array where all values are positive,
-           * use the Math.abs() which returns the absolute value of a number,
-           * which is the number's positive magnitude.
-           */
-          return Math.abs(a.difference_energy) - Math.abs(b.difference_energy);
-        }
+    this.dataSource.data = [...this.dataSource.data].sort((a, b) => {
+      if (priority === 'energy') {
+        /**
+         * Convert an array containing both positive and negative numbers to an array
+         * where all values are positive,
+         * use the Math.abs() which returns the absolute value of a number,
+         */
+        return Math.abs(a.difference_energy) - Math.abs(b.difference_energy);
+      }
 
-        if (priority === 'area') {
-          return Math.abs(a.difference_area) - Math.abs(b.difference_area);
-        }
+      if (priority === 'area') {
+        return Math.abs(a.difference_area) - Math.abs(b.difference_area);
+      }
 
-        // Default sorting behavior if the priority is not recognized
-        return 0;
-      });
+      // Default sorting behavior if the priority is not recognized
+      return 0;
     });
   }
 }
--- src/app/types.ts
+++ src/app/types.ts
@@ -1,10 +1,10 @@
 import { Option } from './datasource';
 
-export interface BestOption {
+export type BestOption = {
   option: Option;
   difference_area: number;
   difference_energy: number;
   quantity: number;
-}
+};
 
 export type Priority = 'area' | 'energy';
